23af021b0ba209c1fb272dcbacaaa93b1c545862
greatoctopusking <great_octopus_king@outlook.com>
2026-04-05 09:40:16 +0800

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,5 @@
 from .trainer import Trainer, create_optimizer, create_scheduler
-from .evaluate import Evaluator, compute_metrics
+from .evaluate import Evaluator, compute_metrics, print_metrics, save_metrics
 from .logger import Logger, setup_logging, log_system_info
 
-__all__ = ['Trainer', 'create_optimizer', 'create_scheduler', 'Evaluator', 'compute_metrics', 'Logger', 'setup_logging', 'log_system_info']
+__all__ = ['Trainer', 'create_optimizer', 'create_scheduler', 'Evaluator', 'compute_metrics', 'print_metrics', 'save_metrics', 'Logger', 'setup_logging', 'log_system_info']
